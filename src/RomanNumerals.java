143e2a016d4edf2431d829090d68c27c62afed5c
pohmelo <mikkovp@hotmail.com>
2018-10-12 17:59:08 +0300

    Assignment done

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,73 @@
 
 public class RomanNumerals {
 	public int convertToInteger(String romanNum) {
-		// To be Implemented
-		return 0;
+		if(romanNum == "I") return 1;
+		int result = 0;
+		int r = 0;
 		
+		if(!checkIfNoIXCM3TimesInARow(romanNum)) return 0;
+		else if(!checkIfNoVLDRepetitions(romanNum)) return 0;
+		else if(!checkIfSubsLegal(romanNum)) return 0;
+		
+		for (int i = 0; i < romanNum.length(); i++){
+	        r++;
+			if(romanNum.charAt(i) == 'I'){
+	        	if(r < romanNum.length() && subCheck(romanNum, i)) result -= 1;
+	        	else result += 1;
+	        }
+	        else if(romanNum.charAt(i) == 'V'){
+	        	result += 5;
+	        }
+	        else if(romanNum.charAt(i) == 'X'){
+	        	if(r < romanNum.length() && subCheck(romanNum, i)) result -= 10;
+	        	else result += 10;
+	        }
+	        else if(romanNum.charAt(i) == 'L'){
+	        	result += 50;
+	        }
+	        else if(romanNum.charAt(i) == 'C'){
+	        	if(r < romanNum.length() && subCheck(romanNum, i)) result -= 100;
+	        	else result += 100;
+	        }
+	        else if(romanNum.charAt(i) == 'D'){
+	        	result += 500;
+	        }
+	        else if(romanNum.charAt(i) == 'M'){
+	        	result += 1000;
+	        }
+	        else{
+	        	return 0;
+	        }
+	    }
+		return result;	
+	}
+	
+	private boolean checkIfNoIXCM3TimesInARow(String str){
+		if(str.contains("IIII") || str.contains("XXXX") || str.contains("CCCC") || str.contains("MMMM")) return false;
+		return true;
+	}
+	
+	private boolean checkIfNoVLDRepetitions(String str){
+		if(str.contains("VV") || str.contains("LL") || str.contains("DD")) return false;
+		return true;
+	}
+	
+	private boolean subCheck(String str, int it){ //returns true if have to subtract
+		char chr = str.charAt(it);
+		if(chr == 'I'){
+			if(str.charAt(it+1) == 'V' || str.charAt(it+1) == 'X') return true;
+		}
+		if(chr == 'X'){
+			if(str.charAt(it+1) == 'L' || str.charAt(it+1) == 'C') return true;
+		}
+		if(chr == 'C'){
+			if(str.charAt(it+1) == 'D' || str.charAt(it+1) == 'M') return true;
+		}
+		return false;
+	}
+	
+	private boolean checkIfSubsLegal(String str){ //return true if subtractions in string are legal
+		if(str.contains("IIV") || str.contains("IIX") || str.contains("XXL") || str.contains("XXC") || str.contains("CCD") || str.contains("CCM")) return false;
+		return true;
 	}
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -3,10 +3,84 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestRomanNumerals {
-
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testIXCMRepetitions(){
+		RomanNumerals roman = new RomanNumerals();
+		assertEquals(0, roman.convertToInteger("IIII"));
+		assertEquals(0, roman.convertToInteger("XXXX"));
+		assertEquals(0, roman.convertToInteger("CCCC"));
+		assertEquals(0, roman.convertToInteger("IMMMM"));
+		assertEquals(3, roman.convertToInteger("III"));
 	}
-
+	
+	@Test
+	public void testVLDRepetitions(){
+		RomanNumerals roman = new RomanNumerals();
+		assertEquals(0, roman.convertToInteger("VV"));
+		assertEquals(0, roman.convertToInteger("LL"));
+		assertEquals(0, roman.convertToInteger("DD"));
+		assertEquals(500, roman.convertToInteger("D"));
+	}
+	
+	@Test
+	public void test1Substractions(){
+		RomanNumerals roman = new RomanNumerals();
+		assertEquals(4, roman.convertToInteger("IV"));
+		assertEquals(9, roman.convertToInteger("IX"));
+		assertEquals(0, roman.convertToInteger("DD"));
+		assertEquals(500, roman.convertToInteger("D"));
+	}
+	
+	@Test
+	public void test2Substractions(){
+		RomanNumerals roman = new RomanNumerals();
+		assertEquals(0, roman.convertToInteger("IIV"));
+		assertEquals(0, roman.convertToInteger("IIIX"));
+	}
+	
+	@Test
+	public void test3Substractions(){
+		RomanNumerals roman = new RomanNumerals();
+		assertFalse(roman.convertToInteger("VX") == 5);
+		assertFalse(roman.convertToInteger("LD") == 450);
+		assertFalse(roman.convertToInteger("DM") == 500);
+		assertFalse(roman.convertToInteger("VL") == 45);
+	}
+	
+	@Test
+	public void testExtras(){
+		RomanNumerals roman = new RomanNumerals();
+		assertEquals(0, roman.convertToInteger("IIII"));
+		assertEquals(1, roman.convertToInteger("I"));
+		assertEquals(2, roman.convertToInteger("II"));
+		assertEquals(3, roman.convertToInteger("III"));
+		assertEquals(4, roman.convertToInteger("IV"));
+		assertEquals(5, roman.convertToInteger("V"));
+		assertEquals(6, roman.convertToInteger("VI"));
+		assertEquals(7, roman.convertToInteger("VII"));
+		assertEquals(8, roman.convertToInteger("VIII"));
+		assertEquals(9, roman.convertToInteger("IX"));
+		assertEquals(10, roman.convertToInteger("X"));
+		assertEquals(20, roman.convertToInteger("XX"));
+		assertEquals(30, roman.convertToInteger("XXX"));
+		assertEquals(40, roman.convertToInteger("XL"));
+		assertEquals(50, roman.convertToInteger("L"));
+		assertEquals(60, roman.convertToInteger("LX"));
+		assertEquals(70, roman.convertToInteger("LXX"));
+		assertEquals(80, roman.convertToInteger("LXXX"));
+		assertEquals(90, roman.convertToInteger("XC"));
+		assertEquals(100, roman.convertToInteger("C"));
+		assertEquals(200, roman.convertToInteger("CC"));
+		assertEquals(300, roman.convertToInteger("CCC"));
+		assertEquals(400, roman.convertToInteger("CD"));
+		assertEquals(500, roman.convertToInteger("D"));
+		assertEquals(600, roman.convertToInteger("DC"));
+		assertEquals(700, roman.convertToInteger("DCC"));
+		assertEquals(800, roman.convertToInteger("DCCC"));
+		assertEquals(900, roman.convertToInteger("CM"));
+		assertEquals(1000, roman.convertToInteger("M"));
+		assertEquals(1984, roman.convertToInteger("MCMLXXXIV"));
+		assertEquals(2014, roman.convertToInteger("MMXIV"));
+	}
 }
